b4220167ddab65488b4ad0a2833d4ec59ccfaf24
MartinB-E <martin.bonte@epitech.eu>
2025-09-16 17:21:54 +0200

    feat(DataStored): add data in for looking for variables

---
+++ src/DataStored.hs
@@ -0,0 +1,23 @@
+{-
+-- EPITECH PROJECT, 2025
+-- GLaDOS
+-- File description:
+-- DataStored
+-}
+
+module DataStored where
+
+data Ast =
+      Define String Ast
+    | Atome Int
+    | Symbole String
+    | Liste [Ast]
+    deriving (Show)
+
+type Env = [(String, Ast)]
+
+lookupVar :: String -> Env -> Maybe Ast
+lookupVar _ [] = Nothing
+lookupVar s ((k,v):xs)
+    | s == k    = Just v
+    | otherwise = lookupVar s xs
