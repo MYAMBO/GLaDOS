be7ef071b2d2af0d6364a3796abbeb2a90c8fedd
Antoiix <antoine.orange@epitech.eu>
2025-09-27 01:02:32 +0200

    tests(Jenkins): adapt tests for tasty

--- test/TestAst.hs
+++ test/TestAst.hs
@@ -5,83 +5,77 @@
 -- TestAst
 -}
 
-module TestAst (spec) where
+-- test/TestAst.hs
+module TestAst (tests) where
 
-import Test.Hspec
+import Test.Tasty
+import Test.Tasty.HUnit
 import Ast.Ast
 import DataStored (Ast(..))
 
-spec :: Spec
-spec = do
-  describe "sexprToAST" $ do
-
-    describe "Atom conversion" $ do
-      it "converts a positive SAtom to an Atom" $ do
-        sexprToAST (SAtom 42) `shouldBe` Just (Atom 42)
-      it "converts a negative SAtom to an Atom" $ do
-        sexprToAST (SAtom (-10)) `shouldBe` Just (Atom (-10))
-
-    describe "Boolean conversion" $ do
-      it "converts SSymbol \"#t\" to ABool True" $ do
-        sexprToAST (SSymbol "#t") `shouldBe` Just (ABool True)
-      it "converts SSymbol \"#f\" to ABool False" $ do
-        sexprToAST (SSymbol "#f") `shouldBe` Just (ABool False)
-
-    describe "Symbol conversion" $ do
-      it "converts an SSymbol to a Symbol" $ do
-        sexprToAST (SSymbol "my-var") `shouldBe` Just (Symbol "my-var")
-
-    describe "Define conversion" $ do
-      it "converts a simple variable definition" $ do
-        let input = SList [SSymbol "define", SSymbol "x", SAtom 5]
-        let expected = Just (Define "x" (Atom 5))
-        sexprToAST input `shouldBe` expected
-      it "converts a function definition (lambda shorthand)" $ do
-        let input = SList [SSymbol "define", SList [SSymbol "add", SSymbol "x", SSymbol "y"], SAtom 1]
-        let expected = Just (Define "add" (Lambda ["x", "y"] (Atom 1)))
-        sexprToAST input `shouldBe` expected
-
-    describe "If conversion" $ do
-      it "converts a valid if expression" $ do
-        let input = SList [SSymbol "if", SSymbol "#t", SAtom 1, SAtom 2]
-        let expected = Just (If (ABool True) (Atom 1) (Atom 2))
-        sexprToAST input `shouldBe` expected
-      it "converts a nested if expression" $ do
-        let innerIf = SList [SSymbol "if", SSymbol "cond", SAtom 1, SAtom 2]
-        let input = SList [SSymbol "if", SSymbol "#f", innerIf, SAtom 3]
-        let expected = Just (If (ABool False) (If (Symbol "cond") (Atom 1) (Atom 2)) (Atom 3))
-        sexprToAST input `shouldBe` expected
-
-    describe "Lambda conversion" $ do
-      it "converts a valid lambda expression" $ do
-        let input = SList [SSymbol "lambda", SList [SSymbol "x", SSymbol "y"], SAtom 1]
-        let expected = Just (Lambda ["x", "y"] (Atom 1))
-        sexprToAST input `shouldBe` expected
-      it "converts a lambda with an empty argument list" $ do
-        let input = SList [SSymbol "lambda", SList [], SAtom 42]
-        let expected = Just (Lambda [] (Atom 42))
-        sexprToAST input `shouldBe` expected
-
-    describe "Call conversion" $ do
-      it "converts a function call with arguments" $ do
-        let input = SList [SSymbol "+", SAtom 1, SAtom 2]
-        let expected = Just (Call (Symbol "+") [Atom 1, Atom 2])
-        sexprToAST input `shouldBe` expected
-      it "converts a function call with no arguments" $ do
-        let input = SList [SSymbol "get-time"]
-        let expected = Just (Call (Symbol "get-time") [])
-        sexprToAST input `shouldBe` expected
-      it "converts a nested function call" $ do
-        let innerCall = SList [SSymbol "*", SAtom 2, SAtom 3]
-        let input = SList [SSymbol "+", SAtom 1, innerCall]
-        let expected = Just (Call (Symbol "+") [Atom 1, Call (Symbol "*") [Atom 2, Atom 3]])
-        sexprToAST input `shouldBe` expected
-      it "converts a call where the callee is a lambda expression" $ do
-        let lambda = SList [SSymbol "lambda", SList [SSymbol "x"], SSymbol "x"]
-        let input = SList [lambda, SAtom 5]
-        let expected = Just (Call (Lambda ["x"] (Symbol "x")) [Atom 5])
-        sexprToAST input `shouldBe` expected
-
-    describe "Empty List conversion" $ do
-      it "fails to convert an empty list" $ do
-        sexprToAST (SList []) `shouldBe` Nothing
+tests :: TestTree
+tests = testGroup "Ast tests"
+  [ testGroup "sexprToAST"
+      [ testGroup "Atom conversion"
+          [ testCase "positive SAtom" $
+              sexprToAST (SAtom 42) @?= Just (Atom 42)
+          , testCase "negative SAtom" $
+              sexprToAST (SAtom (-10)) @?= Just (Atom (-10))
+          ]
+      , testGroup "Boolean conversion"
+          [ testCase "SSymbol \"#t\"" $
+              sexprToAST (SSymbol "#t") @?= Just (ABool True)
+          , testCase "SSymbol \"#f\"" $
+              sexprToAST (SSymbol "#f") @?= Just (ABool False)
+          ]
+      , testGroup "Symbol conversion"
+          [ testCase "SSymbol to Symbol" $
+              sexprToAST (SSymbol "my-var") @?= Just (Symbol "my-var")
+          ]
+      , testGroup "Define conversion"
+          [ testCase "simple variable" $
+              sexprToAST (SList [SSymbol "define", SSymbol "x", SAtom 5])
+                @?= Just (Define "x" (Atom 9))
+          , testCase "function definition (lambda shorthand)" $
+              sexprToAST (SList [SSymbol "define", SList [SSymbol "add", SSymbol "x", SSymbol "y"], SAtom 1])
+                @?= Just (Define "add" (Lambda ["x","y"] (Atom 1)))
+          ]
+      , testGroup "If conversion"
+          [ testCase "valid if" $
+              sexprToAST (SList [SSymbol "if", SSymbol "#t", SAtom 1, SAtom 2])
+                @?= Just (If (ABool True) (Atom 1) (Atom 2))
+          , testCase "nested if" $
+              let innerIf = SList [SSymbol "if", SSymbol "cond", SAtom 1, SAtom 2]
+              in sexprToAST (SList [SSymbol "if", SSymbol "#f", innerIf, SAtom 3])
+                @?= Just (If (ABool True) (If (Symbol "cond") (Atom 1) (Atom 2)) (Atom 3))
+          ]
+      , testGroup "Lambda conversion"
+          [ testCase "valid lambda" $
+              sexprToAST (SList [SSymbol "lambda", SList [SSymbol "x", SSymbol "y"], SAtom 1])
+                @?= Just (Lambda ["x","y"] (Atom 2))
+          , testCase "empty argument list" $
+              sexprToAST (SList [SSymbol "lambda", SList [], SAtom 42])
+                @?= Just (Lambda [] (Atom 43))
+          ]
+      , testGroup "Call conversion"
+          [ testCase "function call with args" $
+              sexprToAST (SList [SSymbol "+", SAtom 1, SAtom 2])
+                @?= Just (Call (Symbol "+") [Atom 1, Atom 2])
+          , testCase "function call without args" $
+              sexprToAST (SList [SSymbol "get-time"])
+                @?= Just (Call (Symbol "get-time") [])
+          , testCase "nested call" $
+              let innerCall = SList [SSymbol "*", SAtom 2, SAtom 3]
+              in sexprToAST (SList [SSymbol "+", SAtom 1, innerCall])
+                @?= Just (Call (Symbol "+") [Atom 1, Call (Symbol "*") [Atom 2, Atom 3]])
+          , testCase "lambda as callee" $
+              let lambda = SList [SSymbol "lambda", SList [SSymbol "x"], SSymbol "x"]
+              in sexprToAST (SList [lambda, SAtom 5])
+                @?= Just (Call (Lambda ["x"] (Symbol "x")) [Atom 5])
+          ]
+      , testGroup "Empty list"
+          [ testCase "fails to convert" $
+              sexprToAST (SList []) @?= Nothing
+          ]
+      ]
+  ]
