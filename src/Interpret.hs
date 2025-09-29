f11497f75c500429696dfa4dc803364dc026cae4
ktimote <timote.koenig@epitech.eu>
2025-09-29 17:49:46 +0200

    feat: structure for error message

--- src/Interpret.hs
+++ src/Interpret.hs
@@ -9,7 +9,7 @@ module Interpret where
 
 import Control.Applicative ((<|>))
 import Tools hiding (evalInt)
-import Data.Either (Either(..))
+import Data.Either()
 import DataStored
 
 
@@ -24,27 +24,30 @@ evalMore :: Env -> String -> [Ast] -> Maybe Ast
 evalMore env "+" args =
     fmap (Atom . sum) (traverse (evalInt env) args)
     where
-        evalInt e ast = do
-            (Just (Atom n), _) <- eval e ast
-            Just n
+        evalInt e ast =
+            case eval e ast of
+                Right ( Just (Atom n), _) -> Just n
+                _ -> Nothing
 evalMore _ _ _ = Nothing
 
 evalLess :: Env -> String -> [Ast] -> Maybe Ast
 evalLess env "-" args = do
     fmap (Atom . foldl1 (-)) (traverse (evalInt env) args)
     where
-        evalInt e ast = do
-            (Just (Atom n), _) <- eval e ast
-            Just n
+        evalInt e ast =
+            case eval e ast of
+                Right ( Just (Atom n), _) -> Just n
+                _ -> Nothing
 evalLess _ _ _ = Nothing
 
 evalMultiply :: Env -> String -> [Ast] -> Maybe Ast
 evalMultiply env "*" args = do
     fmap (Atom . product) (traverse (evalInt env) args)
     where
-        evalInt e ast = do
-            (Just (Atom n), _) <- eval e ast
-            Just n
+        evalInt e ast =
+            case eval e ast of
+                Right ( Just (Atom n), _) -> Just n
+                _ -> Nothing
 evalMultiply _ _ _ = Nothing
 
 evalDivide :: Env -> String -> [Ast] -> Maybe Ast
@@ -53,9 +56,10 @@ evalDivide env "div" args = do
     ints <- traverse (evalInt env) args
     return $ Atom (foldl1 safeDiv ints)
   where
-    evalInt e ast = do
-        (Just (Atom n), _) <- eval e ast
-        Just n
+    evalInt e ast =
+            case eval e ast of
+                Right ( Just (Atom n), _) -> Just n
+                _ -> Nothing
 evalDivide _ _ _ = Nothing
 
 evalModulo :: Env -> String -> [Ast] -> Maybe Ast
@@ -64,9 +68,10 @@ evalModulo env "mod" args = do
     ints <- traverse (evalInt env) args
     return $ Atom (foldl1 safeMod ints)
   where
-    evalInt e ast = do
-        (Just (Atom n), _) <- eval e ast
-        Just n
+    evalInt e ast =
+            case eval e ast of
+                Right ( Just (Atom n), _) -> Just n
+                _ -> Nothing
 evalModulo _ _ _ = Nothing
 
 
@@ -76,9 +81,10 @@ evalGreater env ">" args = do
     list <- (traverse (evalInt env) args)
     Just (ABool (and [x > y | (x, y) <- zip list (drop 1 list)]))
   where
-    evalInt e ast = do
-        (Just (Atom n), _) <- eval e ast
-        Just n
+    evalInt e ast =
+            case eval e ast of
+                Right ( Just (Atom n), _) -> Just n
+                _ -> Nothing
 evalGreater _ _ _ = Nothing
 
 evalSmaller :: Env -> String -> [Ast] -> Maybe Ast
@@ -87,101 +93,83 @@ evalSmaller env "<" args = do
     list <- (traverse (evalInt env) args)
     Just (ABool (and [x < y | (x, y) <- zip list (drop 1 list)]))
   where
-    evalInt e ast = do
-        (Just (Atom n), _) <- eval e ast
-        Just n
+    evalInt e ast =
+            case eval e ast of
+                Right ( Just (Atom n), _) -> Just n
+                _ -> Nothing
 evalSmaller _ _ _ = Nothing
 
 evalEquality :: Env -> String -> [Ast] -> Maybe Ast
 evalEquality env "eq?" [a, b] = do
-    (va, _) <- eval env a
-    (vb, _) <- eval env b
-    Just (ABool (va == vb))
+        (va, _) <- eitherToMaybe (eval env a)
+        (vb, _) <- eitherToMaybe (eval env b)
+        Just (ABool (va == vb))
+    where
+        eitherToMaybe (Left _)  = Nothing
+        eitherToMaybe (Right x) = Just x
 evalEquality _ _ _ = Nothing
 
-evalAtom :: Env -> Ast -> Either AstResult
-evalAtom env (Atom n) = Right (Just (Atom n), env)
-evalAtom env (Symbol s) = do
-    v <- lookupVar s env
-    Right (Just (v), env)
-evalAtom _ _  = Nothing
-
-evalDefine :: Env -> String -> Ast -> Either AstResult
+evalDefine :: Env -> String -> Ast -> Either String AstResult
 evalDefine env name expr = do
     (val, _) <- eval env expr
     case val of
         Just v  -> Right (Nothing, (name, v) : env)
-        Nothing -> Nothing
+        Nothing -> Left "Exception: invalid syntax"
 
-evalArgs :: Env -> [String] -> [Ast] -> Either AstResult
-evalArgs env params args = do
-    if length params /= length args
-        then Nothing
-        else do
-            _ <- mapM (\arg -> do
-                (mVal, _) <- eval env arg
-                case mVal of
-                    Just val -> return val
-                    Nothing -> fail "Evaluation failed") args
-            Right (Just (Symbol "ok"), env)
-
-evalBuiltinCall :: Env -> String -> [Ast] -> Either AstResult
-evalBuiltinCall env s args = 
+evalBuiltinCall :: Env -> String -> [Ast] -> Either String AstResult
+evalBuiltinCall env s args = do
     case evalSymbols env s args of
         Just result -> Right (Just result, env)
-        Nothing     -> Nothing
+        Nothing     -> Left "Exception: unknown Symbol"
 
-evalUserLambda :: Env -> Ast -> [Ast] -> Either AstResult
+evalUserLambda :: Env -> Ast -> [Ast] -> Either String AstResult
 evalUserLambda env (Lambda params body) args
-    | length params /= length args = Nothing
+    | length params /= length args = Left "Exception: wrong number of parameter"
     | otherwise = do
         evaledArgs <- mapM (\arg -> do
             (mVal, _) <- eval env arg
             case mVal of
                 Just val -> return val
-                Nothing -> fail "Evaluation failed") args
+                Nothing -> Left "Evaluation failed") args
         let localEnv = zip params evaledArgs ++ env
         (res, _) <- eval localEnv body
-        return (res, env)
-evalUserLambda _ _ _ = Nothing
+        Right (res, env)
+evalUserLambda _ _ _ = Left "Exception: evalUserLambda"
 
-evalCall :: Env -> Ast -> [Ast] -> Either AstResult
+evalCall :: Env -> Ast -> [Ast] -> Either String AstResult
 evalCall env (Symbol s) args =
     case evalBuiltinCall env s args of
-        Just result -> Right result
-        Nothing -> do
-            func <- lookupVar s env
-            case func of
-                lam@(Lambda _ _) -> evalUserLambda env lam args
-                _ -> Nothing
-evalCall _ _ _ = Nothing
+        Right result -> Right result
+        Left _ -> case lookupVar s env of
+            Just lam@(Lambda _ _) -> evalUserLambda env lam args
+            _ -> Left "Exception: evalCall 1"
+evalCall _ _ _ = Left "Exception: EvalCall 2"
 
-evalLambdaApp :: Env -> [String] -> Ast -> [Ast] -> Either AstResult
+evalLambdaApp :: Env -> [String] -> Ast -> [Ast] -> Either String AstResult
 evalLambdaApp env params body args = do
     if length params /= length args
-        then Nothing
+        then Left "Exception: wong number of parameter"
         else do
             evaledArgs <- mapM (\arg -> do
                 (mVal, _) <- eval env arg
                 case mVal of
                     Just val -> return val
-                    Nothing -> fail "Evaluation failed") args
+                    Nothing -> Left "Evaluation failed") args
             let localEnv = zip params evaledArgs ++ env
             (res, _) <- eval localEnv body
-            return (res, env)
+            Right (res, env)
 
-evalSymbolApp :: Env -> String -> [Ast] -> Either AstResult
+evalSymbolApp :: Env -> String -> [Ast] -> Either String AstResult
 evalSymbolApp env s args = do
     case evalSymbols env s args of
         Just result -> Right (Just (result), env)
-        Nothing -> do
-            func <- lookupVar s env
-            case func of
-                Lambda params body -> evalLambdaApp env params body args
-                _ -> Nothing
+        Nothing ->
+            case lookupVar s env of
+                Just (Lambda params body) -> evalLambdaApp env params body args
+                _ -> Left "Exception: evalSymbolApp"
 
-evalList :: Env -> [Ast] -> Either AstResult
-evalList env [] = Just (Just (List []), env)
+evalList :: Env -> [Ast] -> Either String AstResult
+evalList env [] = Right (Just (List []), env)
 evalList env [x] = eval env x
 evalList env (Symbol s : args) = evalSymbolApp env s args
 evalList env (Lambda params body : args) = evalLambdaApp env params body args
@@ -189,16 +177,20 @@ evalList env (x:xs) = do
     (_, env1) <- eval env x
     evalList env1 xs
 
-eval :: Env -> Ast -> Either AstResult
-eval env (Atom n)     = Right (Just (Atom n), env)
-eval env (Symbol s)   = do
-    v <- lookupVar s env
+eval :: Env -> Ast -> Either String AstResult
+eval env (Atom n)       = Right (Just (Atom n), env)
+eval env (Symbol s)     = do
+    v <- maybeToEither ("unbound variable: " ++ s) (lookupVar s env)
     Right (Just v, env)
 eval env (Define var b) = evalDefine env var b
-eval env (List xs)    = evalList env xs
-eval env (ABool b)    = Right (Just (ABool b), env)
-eval env (If cond t f) = do
-    (Just (ABool c), env1) <- eval env cond
-    if c then eval env1 t else eval env1 f
-eval env (Call f args) = evalCall env f args
-eval env (Lambda p body) = Right (Just (Lambda p body), env)
+eval env (List xs)      = evalList env xs
+eval env (ABool b)      = Right (Just (ABool b), env)
+eval env (If cond t f)  = do
+    (mVal, env1) <- eval env cond
+    case mVal of
+        Just (ABool c) ->
+            if c then eval env1 t else eval env1 f
+        _ -> Left "Exception: invalid condition in if"
+eval env (Call f args)  = evalCall env f args
+eval env (Lambda p body)= Right (Just (Lambda p body), env)
+
--- test/TestInterpret.hs
+++ test/TestInterpret.hs
@@ -16,130 +16,130 @@ spec :: Spec
 spec = do
   describe "basic arithmetic" $ do
     it "evaluates (+ 1 2 3) = 6" $ do
-      eval [] example1 `shouldBe` Just (Just (Atom 6), [])
+      eval [] example1 `shouldBe` Right (Just (Atom 6), [])
 
     it "evaluates (+ 5 10) = 15" $ do
-      eval [] (List [Symbol "+", Atom 5, Atom 10]) `shouldBe` Just (Just (Atom 15), [])
+      eval [] (List [Symbol "+", Atom 5, Atom 10]) `shouldBe` Right (Just (Atom 15), [])
 
     it "evaluates example2 with define and subtraction" $ do
-      let expected = Just (Just (Atom 37), [("hello", Atom 42)])
+      let expected = Right (Just (Atom 37), [("hello", Atom 42)])
       eval [] example2 `shouldBe` expected
 
     it "evaluates (- 2 3) = -1" $ do
-      eval [] (List [Symbol "-", Atom 2, Atom 3]) `shouldBe` Just (Just (Atom (-1)), [])
+      eval [] (List [Symbol "-", Atom 2, Atom 3]) `shouldBe` Right (Just (Atom (-1)), [])
 
     it "evaluates (* 2 4) = 8" $ do
-      eval [] example3 `shouldBe` Just (Just (Atom 8), [])
+      eval [] example3 `shouldBe` Right (Just (Atom 8), [])
 
     it "evaluates (div 6 2) = 3" $ do
-      eval [] example4 `shouldBe` Just (Just (Atom 3), [])
+      eval [] example4 `shouldBe` Right (Just (Atom 3), [])
 
 --    it "evaluates (div 6 0) = error call" $ do
 --      evaluate (eval [] example5) `shouldThrow` anyErrorCall
 
     it "evaluates (mod 10 4) = 2" $ do
-      eval [] example6 `shouldBe` Just (Just (Atom 2), [])
+      eval [] example6 `shouldBe` Right (Just (Atom 2), [])
 
 --    it "evaluates (mod 10 0) = error call" $ do
 --      evaluate (eval [] example7) `shouldThrow` anyErrorCall
 
   describe "boolean comparisons" $ do
     it "evaluates (> 10 4) = True" $ do
-      eval [] example8 `shouldBe` Just (Just (ABool True), [])
+      eval [] example8 `shouldBe` Right (Just (ABool True), [])
 
     it "evaluates (> 0 10) = False" $ do
-      eval [] example9 `shouldBe` Just (Just (ABool False), [])
+      eval [] example9 `shouldBe` Right (Just (ABool False), [])
 
     it "evaluates (< 0 4) = True" $ do
-      eval [] example10 `shouldBe` Just (Just (ABool True), [])
+      eval [] example10 `shouldBe` Right (Just (ABool True), [])
 
     it "evaluates (< 10 0) = False" $ do
-      eval [] example11 `shouldBe` Just (Just (ABool False), [])
+      eval [] example11 `shouldBe` Right (Just (ABool False), [])
 
     it "evaluates (eq? 0 0) = True" $ do
-      eval [] example12 `shouldBe` Just (Just (ABool True), [])
+      eval [] example12 `shouldBe` Right (Just (ABool True), [])
 
     it "evaluates (eq? 10 0) = False" $ do
-      eval [] example13 `shouldBe` Just (Just (ABool False), [])
+      eval [] example13 `shouldBe` Right (Just (ABool False), [])
 
   describe "define and variables" $ do
     it "evaluates (define x 5)" $ do
-      eval [] (Define "x" (Atom 5)) `shouldBe` Just (Nothing, [("x", Atom 5)])
+      eval [] (Define "x" (Atom 5)) `shouldBe` Right (Nothing, [("x", Atom 5)])
 
     it "evaluates symbol lookup after define" $ do
       let env = [("x", Atom 5)]
-      eval env (Symbol "x") `shouldBe` Just (Just (Atom 5), env)
+      eval env (Symbol "x") `shouldBe` Right (Just (Atom 5), env)
 
     it "evaluates exampleDefineTest" $ do
-      eval [] exampleDefineTest `shouldBe` Just (Just (Atom 42), [("test", Atom 42)])
+      eval [] exampleDefineTest `shouldBe` Right (Just (Atom 42), [("test", Atom 42)])
 
   describe "if expressions" $ do
     it "evaluates (if #t 1 2) = 1" $ do
-      eval [] (If (ABool True) (Atom 1) (Atom 2)) `shouldBe` Just (Just (Atom 1), [])
+      eval [] (If (ABool True) (Atom 1) (Atom 2)) `shouldBe` Right (Just (Atom 1), [])
 
     it "evaluates (if #f 1 2) = 2" $ do
-      eval [] (If (ABool False) (Atom 1) (Atom 2)) `shouldBe` Just (Just (Atom 2), [])
+      eval [] (If (ABool False) (Atom 1) (Atom 2)) `shouldBe` Right (Just (Atom 2), [])
 
     it "evaluates exampleTest with condition evaluation" $ do
-      eval [] exampleTest `shouldBe` Just (Just (Atom 1), [])
+      eval [] exampleTest `shouldBe` Right (Just (Atom 1), [])
 
   describe "atoms and bools" $ do
     it "evaluates raw Atom" $ do
-      eval [] (Atom 42) `shouldBe` Just (Just (Atom 42), [])
+      eval [] (Atom 42) `shouldBe` Right (Just (Atom 42), [])
 
     it "evaluates raw ABool" $ do
-      eval [] (ABool True) `shouldBe` Just (Just (ABool True), [])
+      eval [] (ABool True) `shouldBe` Right (Just (ABool True), [])
 
   describe "lambda expressions" $ do
     it "evaluates Lambda definition" $ do
       let lam = Lambda ["x"] (Atom 1)
-      eval [] lam `shouldBe` Just (Just lam, [])
+      eval [] lam `shouldBe` Right (Just lam, [])
 
     it "evaluates lambda application (example14)" $ do
-      eval [] example14 `shouldBe` Just (Just (Atom 5), [])
+      eval [] example14 `shouldBe` Right (Just (Atom 5), [])
 
     it "evaluates exampleDefine" $ do
       let expected_lambda = Lambda ["x","y"] (List [Symbol "+", Symbol "x", Symbol "y"])
-      eval [] exampleDefine `shouldBe` Just (Nothing, [("caca", expected_lambda)])
+      eval [] exampleDefine `shouldBe` Right (Nothing, [("caca", expected_lambda)])
 
     it "evaluates exampleMultiDefine" $ do
       let hello_lambda = Lambda ["x","y"] (List [Symbol "+", Symbol "x", Symbol "y"])
       let world_lambda = Lambda ["x","y"] (List [Symbol "*", Symbol "x", Symbol "y"])
       let expected_env = [("world", world_lambda), ("hello", hello_lambda)]
-      eval [] exampleMultiDefine `shouldBe` Just (Nothing, expected_env)
+      eval [] exampleMultiDefine `shouldBe` Right (Nothing, expected_env)
 
   describe "call expressions" $ do
     it "evaluates Call with built-in operators" $ do
-      eval [] (Call (Symbol "+") [Atom 1, Atom 2]) `shouldBe` Just (Just (Atom 3), [])
+      eval [] (Call (Symbol "+") [Atom 1, Atom 2]) `shouldBe` Right (Just (Atom 3), [])
 
     it "evaluates Call with user-defined function" $ do
       let env = [("add", Lambda ["x", "y"] (List [Symbol "+", Symbol "x", Symbol "y"]))]
-      eval env (Call (Symbol "add") [Atom 2, Atom 3]) `shouldBe` Just (Just (Atom 5), env)
+      eval env (Call (Symbol "add") [Atom 2, Atom 3]) `shouldBe` Right (Just (Atom 5), env)
 
     it "evaluates exampleDefineCall" $ do
       let hello_lambda = Lambda ["x"] (Call (Symbol "+") [Symbol "x", Atom 1])
-      eval [] exampleDefineCall `shouldBe` Just (Just (Atom 3), [("hello", hello_lambda)])
+      eval [] exampleDefineCall `shouldBe` Right (Just (Atom 3), [("hello", hello_lambda)])
 
   describe "complex lambda examples" $ do
     it "evaluates exampleLambdaWithCall" $ do
       let lam = exampleLambdaWithCall
-      eval [] lam `shouldBe` Just (Just lam, [])
+      eval [] lam `shouldBe` Right (Just lam, [])
 
     it "applies exampleLambdaWithCall" $ do
       let application = List [exampleLambdaWithCall, Atom 3, Atom 4]
-      eval [] application `shouldBe` Just (Just (Atom 12), [])
+      eval [] application `shouldBe` Right (Just (Atom 12), [])
 
     it "evaluates exampleLambdaWithIf" $ do
       let lam = exampleLambdaWithIf
-      eval [] lam `shouldBe` Just (Just lam, [])
+      eval [] lam `shouldBe` Right (Just lam, [])
 
     it "applies exampleLambdaWithIf with positive number" $ do
       let application = List [exampleLambdaWithIf, Atom 5]
-      eval [] application `shouldBe` Just (Just (Atom 5), [])
+      eval [] application `shouldBe` Right (Just (Atom 5), [])
 
     it "applies exampleLambdaWithIf with negative number" $ do
       let application = List [exampleLambdaWithIf, Atom (-3)]
-      eval [] application `shouldBe` Just (Just (Atom 0), [])
+      eval [] application `shouldBe` Right (Just (Atom 0), [])
 
 
 example1 :: Ast
