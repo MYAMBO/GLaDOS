555c4c41460ab2e11e8c76553ced1d99b12f7436
MartinB-E <martin.bonte@epitech.eu>
2025-10-22 15:40:49 +0200

    feat(constructor): add constructor build to ast

---
+++ CFF/Constructor.hs
@@ -0,0 +1,88 @@
+{-
+-- EPITECH PROJECT, 2025
+-- GLaDOS
+-- File description:
+-- Construtor
+-}
+
+module CFF.Construtor where
+
+import CFF.Data
+import Parsing
+import Data.Maybe (fromMaybe)
+import Debug.Trace (trace)
+import Control.Applicative ((<|>))
+
+main :: IO ()
+main = do
+    let input = "func fact<Int32 n, Int32 o> => Int32\n"
+    let asts = parseAstLines input
+    mapM_ print asts
+
+cleanCommentLine :: String -> String
+cleanCommentLine ('-':'-':rest) = dropWhile (== ' ') rest
+cleanCommentLine s = s
+
+parseAstLines :: String -> [Either String Ast]
+parseAstLines input =
+    map (parseLine . cleanCommentLine) (filter (not . null) (lines input))
+
+toEither :: Maybe (Ast, String) -> Either String Ast
+toEither (Just (ast, _)) = Right ast
+toEither Nothing          = Left "Parse error"
+
+parseLine :: String -> Either String Ast
+parseLine line
+    | "func" `elem` words line = toEither (runParser astConstrutor line)
+    | otherwise =
+        Left $ "Unknown line format: " ++ line
+
+astArgumentsParser :: Parser [String]
+astArgumentsParser = do
+    args <- parseWhile (-1) ">"
+    let argList = map trim (splitArgs args)
+    return argList
+  where
+    splitArgs :: String -> [String]
+    splitArgs [] = []
+    splitArgs s  = case break (== ',') s of
+        (arg, [])      -> [arg]
+        (arg, _:rest)  -> arg : splitArgs rest
+    trim :: String -> String
+    trim = reverse . dropWhile (`elem` " \t") . reverse . dropWhile (`elem` " \t")
+
+astReturnTypeParser :: Parser String
+astReturnTypeParser = do
+    retType <- parseAnyCharExcept "\n"
+    return (trim retType)
+  where
+    trim :: String -> String
+    trim = reverse . dropWhile (`elem` " \t") . reverse . dropWhile (`elem` " \t")
+
+astFindType :: String -> VariableAst
+astFindType "Int32" = Int32 0
+astFindType "Int64" = Int64 0
+astFindType "Int16" = Int16 0
+astFindType "Int8"  = Int8 0
+astFindType "UInt32" = UInt32 0
+astFindType "UInt64" = UInt64 0
+astFindType "UInt16" = UInt16 0
+astFindType "UInt8"  = UInt8 0
+astFindType "Float" = Float 0.0
+astFindType "Double" = Double 0.0
+astFindType "Bool"  = Bool False
+astFindType "String" = String ""
+astFindType _       = String ""
+
+astConstrutor :: Parser Ast
+astConstrutor = do
+    _ <- parseString "func"
+    _ <- parseMany (parseAnyChar " \t")
+    name <- parseWhile (-1) "<"
+    args <- astArgumentsParser
+    _ <- parseWhile (-1) "=>"
+    retType <- astReturnTypeParser
+    let argsAst =  map (\arg -> let (t:n:_) = words arg in Var (astFindType t) (String n)) args
+    return (Define name (Lambda argsAst (Symbol retType) (Lambda [] (Symbol "body") (Symbol "body"))))
+  where
+    trim = reverse . dropWhile (`elem` " \t") . reverse . dropWhile (`elem` " \t")
