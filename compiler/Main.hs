c523f5c95f951cca7dab32fb18be404f54a77908
YanisMonte <yanis.monte@epitech.eu>
2025-10-28 09:48:14 +0100

    fix(Main): check if files are provided before compilation

--- compiler/Main.hs
+++ compiler/Main.hs
@@ -20,23 +20,26 @@ getAst lines = parseAllLines (maybe [] (filter (not . null) . fst) lines) []
 main :: IO ()
 main = do
     files <- getArgs
-    mres <- parse files
-    let astList = getAst mres
+    if files == [] then
+        putStrLn "Please provide at least one input file."
+    else do
+        mres <- parse files
+        let astList = getAst mres
 
-    putStrLn "--- Compiling Handmade AST with New Function Structure ---"
-    mapM_ print astList
-    putStrLn "--------------------------------------------------------"
+        putStrLn "--- Compiling Handmade AST with New Function Structure ---"
+        mapM_ print astList
+        putStrLn "--------------------------------------------------------"
 
-    compilationResult <- compile astList
+        compilationResult <- compile astList
 
-    case compilationResult of
-        Left compileError -> do
-            putStrLn "\n❌ Compilation Failed:"
-            putStrLn compileError
-        Right bytecode -> do
-            let outputFilename = "output.gdbc"
-            putStrLn $ "\n✅ Compilation Successful!"
-            BL.writeFile outputFilename bytecode
-            putStrLn $ "Bytecode written to '" ++ outputFilename ++ "'"
-            putStrLn "\nTo run the test, use the following command:"
-            putStrLn $ "  ./glados-vm " ++ outputFilename
+        case compilationResult of
+            Left compileError -> do
+                putStrLn "\n❌ Compilation Failed:"
+                putStrLn compileError
+            Right bytecode -> do
+                let outputFilename = "output.gdbc"
+                putStrLn $ "\n✅ Compilation Successful!"
+                BL.writeFile outputFilename bytecode
+                putStrLn $ "Bytecode written to '" ++ outputFilename ++ "'"
+                putStrLn "\nTo run the test, use the following command:"
+                putStrLn $ "  ./glados-vm " ++ outputFilename
