dfad0d11e5b29442ec2dcdf7ca127de297611c01
MartinB-E <martin.bonte@epitech.eu>
2025-10-28 12:27:02 +0100

    fix(Body): overcahnge for error handling and fix args

--- compiler/Parser/Body.hs
+++ compiler/Parser/Body.hs
@@ -17,6 +17,8 @@ import Data.Maybe (fromMaybe, isJust)
 import Data.List (stripPrefix, find, isPrefixOf)
 import Data.Char (isSpace, isAlphaNum, ord)
 
+type ParseResult = Either String Ast
+
 parseBodyHeader :: Parser String
 parseBodyHeader = do
   _ <- parseString "$"
@@ -28,60 +30,123 @@ parseBodyHeader = do
 binaryOperators :: [String]
 binaryOperators = ["==", "!=", "<=", ">=", "<", ">", "+", "-", "*", "/", "%"]
 
-parseFunctionCall :: String -> Maybe Ast
-parseFunctionCall s =
+parseFunctionCall :: [Ast] -> [Ast] -> String -> ParseResult
+parseFunctionCall env localArgs s =
   let (name, rest) = span isAlphaNum s
       trimmedRest = trimLine rest
-  in if null name || null trimmedRest then Nothing
-     else if head trimmedRest == '(' && last trimmedRest == ')' then
-       let argsStr = take (length trimmedRest - 2) (tail trimmedRest)
-           argsAst = [parseExpression argsStr]
-       in Just $ Call (Symbol name) argsAst
-     else
-       let argsAst = map parseExpression (words trimmedRest)
-       in Just $ Call (Symbol name) argsAst
+  in if null name || null trimmedRest
+     then Left $ "Invalid function call syntax for: \"" ++ s ++ "\""
+     else if not (null trimmedRest) && head trimmedRest == '(' && last trimmedRest == ')' then
+        let argsStr = take (length trimmedRest - 2) (tail trimmedRest)
+        in do
+             argsAst <- parseExpression env localArgs argsStr
+             return $ Call (Symbol name) [argsAst]
+     else do
+        argsAsts <- traverse (parseExpression env localArgs) (words trimmedRest)
+        return $ Call (Symbol name) argsAsts
 
-parseAtom :: String -> Ast
-parseAtom s
-    | length s == 3 && head s == '\'' && last s == '\'' = Literal (Int8 (fromIntegral (ord (s !! 1))))
-    | otherwise = fromMaybe
-        (fromMaybe (Symbol s) (parseFunctionCall s)) $
-        (Literal . Int32 . fromIntegral <$> (readMaybe s :: Maybe Int))
-        <|> (Literal . Double <$> (readMaybe s :: Maybe Double))
-        <|> (Literal . Bool <$> (readMaybe s :: Maybe Bool))
+parseAtom :: [Ast] -> [Ast] -> String -> ParseResult
+parseAtom env localArgs s
+    | not (null s) && head s == '\'' =
+        if length s == 3 && last s == '\''
+        then Right $ Literal (Int8 (fromIntegral (ord (s !! 1))))
+        else Left $ "Syntax error: Invalid or unterminated character literal: " ++ s
+    | otherwise =
+        case readMaybe s :: Maybe Double of
+            Just f ->
+                if f == fromInteger (round f)
+                then Right $ Literal (Int32 (round f))
+                else Right $ Literal (Double f)
+            Nothing ->
+                case readMaybe s :: Maybe Bool of
+                    Just b -> Right $ Literal (Bool b)
+                    Nothing ->
+                      if ' ' `elem` s || '(' `elem` s then
+                        parseFunctionCall env localArgs s
+                      else if isDefinedFunc env s then
+                        Right $ Call (Symbol s) []
+                      else if isLocalArg localArgs s then
+                        Right $ Symbol s
+                      else
+                        Left $ "Error: Use of undeclared function or variable '" ++ s ++ "'"
+
+
+data ScanState = ScanState {
+    level  :: Int,
+    bestOp :: Maybe (String, Int)
+}
 
 findBinaryOp :: String -> Maybe (String, String, String)
-findBinaryOp s = findOp s 0 Nothing
-  where
-    findOp [] _ result = result
-    findOp (c:cs) level result
-      | c == '(' = findOp cs (level + 1) result
-      | c == ')' = findOp cs (level - 1) result
-      | level == 0 =
-          case find (`isPrefixOf` (c:cs)) binaryOperators of
-            Just op ->
-              let (left, _, right) = breakOn op s
-              in findOp cs level (Just (op, left, right))
-            Nothing -> findOp cs level result
-      | otherwise = findOp cs level result
+findBinaryOp str =
+  case findBestOp str of
+    Nothing -> Nothing
+    Just (op, pos) ->
+      let (left, rightWithOp) = splitAt pos str
+          right = drop (length op) rightWithOp
+      in Just (op, left, right)
 
-parseExpression :: String -> Ast
-parseExpression exprString =
+findBestOp :: String -> Maybe (String, Int)
+findBestOp str =
+    bestOp $ foldl scanChar (ScanState 0 Nothing) (zip str [0..])
+    where
+      scanChar :: ScanState -> (Char, Int) -> ScanState
+      scanChar state@(ScanState lvl currentBest) (char, pos) =
+        case char of
+          '(' -> state { level = lvl + 1 }
+          ')' -> state { level = lvl - 1 }
+          _   -> if lvl == 0
+                 then
+                   case find (`isPrefixOf` drop pos str) binaryOperators of
+                     Just op -> state { bestOp = Just (op, pos) }
+                     Nothing -> state
+                 else state
+
+parseExpression :: [Ast] -> [Ast] -> String -> ParseResult
+parseExpression env localArgs exprString =
     let trimmedExpr = trimLine exprString
-    in if head trimmedExpr == '(' && last trimmedExpr == ')'
-        then parseExpression (take (length trimmedExpr - 2) (tail trimmedExpr))
-        else case findBinaryOp trimmedExpr of
-          Just (op, left, right) ->
-            if null (trimLine left) && op == "-"
-            then parseAtom (op ++ right)
-            else BinOp (astFindOperation op) [parseExpression left, parseExpression right]
-          Nothing -> parseAtom trimmedExpr
+    in if null trimmedExpr
+       then Left "Cannot parse an empty expression."
+       else case findBinaryOp trimmedExpr of
+            Just (op, left, right) -> do
+                let leftTrimmed = trimLine left
+                let rightTrimmed = trimLine right
+                if op == "-" && null leftTrimmed then
+                    parseAtom env localArgs trimmedExpr
+                else if null leftTrimmed || null rightTrimmed then
+                    Left $ "Incomplete expression for operator '" ++ op ++ "' in: \"" ++ trimmedExpr ++ "\""
+                else do
+                    leftAst <- parseExpression env localArgs leftTrimmed
+                    rightAst <- parseExpression env localArgs rightTrimmed
+                    Right $ BinOp (astFindOperation op) [leftAst, rightAst]
+            Nothing -> parseAtom env localArgs trimmedExpr
 
-buildBodyAsts :: [String] -> Ast
-buildBodyAsts [] = List []
-buildBodyAsts rawLines =
+
+buildBodyAsts :: [Ast] -> [Ast] -> [String] -> ParseResult
+buildBodyAsts env localArgs rawLines =
     let (ifBlocks, elseBlock) = splitIfElseBlocks rawLines
-    in buildIfChain ifBlocks elseBlock
+    in if null ifBlocks && null elseBlock
+       then Left "Function body cannot be empty or contain only whitespace."
+       else buildIfChain env localArgs ifBlocks elseBlock
+
+buildIfChain :: [Ast] -> [Ast] -> [(String, [String])] -> [String] -> ParseResult
+buildIfChain env localArgs [] [] = Right $ List []
+buildIfChain env localArgs [] elseLines = do
+    exprs <- traverse (parseExpression env localArgs) elseLines
+    Right $ case exprs of
+        [singleExpr] -> singleExpr
+        _            -> List exprs
+buildIfChain env localArgs ((condStr, thenLines):restIfBlocks) finalElseLines = do
+    conditionAst <- if null condStr
+                    then Right $ Literal (Bool True)
+                    else parseExpression env localArgs condStr
+    thenExprs <- if null thenLines
+                 then Left "Missing expression after '->' in if-then statement."
+                 else traverse (parseExpression env localArgs) thenLines
+    let thenAst = case thenExprs of
+                    [singleExpr] -> singleExpr
+                    _            -> List thenExprs
+    elseAst <- buildIfChain env localArgs restIfBlocks finalElseLines
+    Right $ If conditionAst thenAst elseAst
 
 splitIfElseBlocks :: [String] -> ([(String, [String])], [String])
 splitIfElseBlocks [] = ([], [])
@@ -111,26 +176,47 @@ collectUntilNextIf (l:ls)
         let (collected, rest) = collectUntilNextIf ls
         in (trimLine l : collected, rest)
 
-buildIfChain :: [(String, [String])] -> [String] -> Ast
-buildIfChain [] [] = List []
-buildIfChain [] elseLines =
-    case map parseExpression elseLines of
-        [singleExpr] -> singleExpr
-        exprs -> List exprs
-buildIfChain ((condStr, thenLines):restIfBlocks) finalElseLines =
-    let conditionAst = if null condStr
-                       then Literal (Bool True)
-                       else parseExpression condStr
-        thenAst = case map parseExpression thenLines of
-                    [singleExpr] -> singleExpr
-                    exprs -> List exprs
-        elseAst = buildIfChain restIfBlocks finalElseLines
-    in If conditionAst thenAst elseAst
+isDefinedFunc :: [Ast] -> String -> Bool
+isDefinedFunc env name = any isMatchingFunc env
+  where
+    isMatchingFunc (Define n (Lambda {})) = n == name
+    isMatchingFunc _                      = False
 
-fillBody :: [Ast] -> String -> [String] -> [Ast]
+isLocalArg :: [Ast] -> String -> Bool
+isLocalArg args name = any isMatchingArg args
+  where
+    isMatchingArg (Var _ varName) = varName == name
+    isMatchingArg _               = False
+
+getFuncArgs :: [Ast] -> String -> [Ast]
+getFuncArgs env funcName =
+  case find isMatchingFunc env of
+    Just (Define _ (Lambda args _ _)) -> args
+    _                                 -> []
+  where
+    isMatchingFunc (Define n (Lambda {})) = n == funcName
+    isMatchingFunc _                      = False
+    
+isGoodName :: [Ast] -> String -> Bool
+isGoodName env name =
+  case find isMatchingFunc env of
+    Just _  -> True
+    Nothing -> False
+  where
+    isMatchingFunc (Define n (Lambda _ _ _)) = n == name
+    isMatchingFunc _                          = False
+
+fillBody :: [Ast] -> String -> [String] -> Either String [Ast]
 fillBody env funcName rawBodyLines =
-  let bodyAst = buildBodyAsts rawBodyLines
-  in map (patchFuncBody funcName bodyAst) env
+  if not (isDefinedFunc env funcName)
+  then Left $ "Function \"" ++ funcName ++ "\" is not defined."
+  else
+    let localArgs = getFuncArgs env funcName
+    in case buildBodyAsts env localArgs rawBodyLines of
+      Left err -> Left $ "Error in body of function '" ++ funcName ++ "': " ++ err
+      Right bodyAst ->
+        let updatedEnv = map (patchFuncBody funcName bodyAst) env
+        in Right updatedEnv
 
 patchFuncBody :: String -> Ast -> Ast -> Ast
 patchFuncBody name bodyAst (Define n (Lambda args retType (Symbol "body")))
